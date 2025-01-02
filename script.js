937b58b458224b7ac41d9cdb67eba53f206def6c
MarieWager <maje0026@stud.kea.dk>
2025-01-02 21:17:14 +0100

    forside, header, footer

---
+++ script.js
@@ -0,0 +1,5 @@
+// Burger menu funktionalitet
+document.getElementById("burgerMenu").addEventListener("click", function () {
+  const menu = document.getElementById("menuList2");
+  menu.classList.toggle("active");
+});
